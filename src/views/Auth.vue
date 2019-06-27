87f0b1468f8d393bbe41aae7707c806f41da6b1e
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2019-06-27 18:16:06 +0200

    Export firebase auth ui to firebaseAuthHelper

--- src/views/Auth.vue
+++ src/views/Auth.vue
@@ -7,8 +7,6 @@
 
 <script lang="ts">
 import { FirebaseAuthHelper } from "ahpaa-core";
-import firebase from "firebase/app";
-import firebaseui from "firebaseui";
 import Vue from "vue";
 
 import { routes } from "../routes";
@@ -16,12 +14,7 @@ import { routes } from "../routes";
 export default Vue.extend({
     name: "auth",
     mounted() {
-        const uiConfig = {
-            signInSuccessUrl: routes.home.path,
-            signInOptions: FirebaseAuthHelper.getSignInProviders(),
-        };
-        const ui = new firebaseui.auth.AuthUI(FirebaseAuthHelper.doAuth());
-        ui.start("#firebaseui-auth-container", uiConfig);
+        FirebaseAuthHelper.startFirebaseAuthUI("#firebaseui-auth-container", routes.home.path);
     },
 });
 </script>
