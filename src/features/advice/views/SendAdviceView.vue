c47e0390fc1f89e38cd2559ffe1af5db665b20c4
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2019-08-29 17:49:15 +0200

    refactor: views/SendAdvice -> features/advice/views/SendAdviceView

--- src/features/advice/views/SendAdviceView.vue
+++ src/features/advice/views/SendAdviceView.vue
@@ -1,15 +1,15 @@
 <template>
-  <div class="home">
-    <h1 class="mb-3 ml-2">{{ text.sendAdvice }}</h1>
-    <add-advice-panel/>
-  </div>
+    <div class="home">
+        <h1 class="mb-3 ml-2">{{ text.sendAdvice }}</h1>
+        <add-advice-panel />
+    </div>
 </template>
 
 <script lang="ts">
 import AddAdvicePanel from "@/components/add-advice/AddAdvicePanel.vue";
 import Vue from "vue";
 
-import { labels } from "../global";
+import { labels } from "../../../global";
 
 export default Vue.extend({
     name: "send-advice",
--- src/plugins/router.ts
+++ src/plugins/router.ts
@@ -4,8 +4,8 @@ import Router from "vue-router";
 import { Configuration } from "../config/Configuration";
 import { routes } from "../config/routes";
 import AdviceListView from "../features/advice/views/AdviceListView.vue";
+import SendAdvice from "../features/advice/views/SendAdviceView.vue";
 import Chat from "../features/discussion/views/DiscussionView.vue";
-import SendAdvice from "../views/SendAdvice.vue";
 
 Vue.use(Router);
 
