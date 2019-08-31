b77dcb292d1a605e405c727bbfb05fac693103d7
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2019-08-31 19:23:34 +0200

    feat: introduce FabActivatedDialog

---
+++ src/components/common/FabActivatedDialogCard.vue
@@ -0,0 +1,43 @@
+<template>
+    <generic-activated-dialog-card ref="genericActivatedDialog" :tooltip="tooltip" :max-width="maxWidth" :dialog-title="dialogTitle">
+        <template v-slot:button="{ on }">
+            <v-btn
+                    class="dialog-fab"
+                    fixed
+                    dark
+                    fab
+                    large
+                    bottom
+                    right
+                    color="accent"
+                    @click.stop="openDialog()"
+                    v-on="on"
+            >
+                <v-icon>fa-plus</v-icon>
+            </v-btn>
+        </template>
+        <slot></slot>
+
+    </generic-activated-dialog-card>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+    props: {
+        dialogTitle: String,
+        tooltip: String,
+        maxWidth: Number,
+    },
+    methods: {
+        openDialog() { (this.$refs.genericActivatedDialog as any).openDialog(); },
+    },
+});
+</script>
+<style scoped lang="scss">
+    .dialog-fab {
+        margin-bottom: 5em;
+        margin-right: 4em;
+    }
+</style>
--- src/components/common/common_components.ts
+++ src/components/common/common_components.ts
@@ -1,8 +1,10 @@
 import Vue from "vue";
 
+import FabActivatedDialogCard from "./FabActivatedDialogCard.vue";
 import GenericActivatedDialogCard from "./GenericActivatedDialogCard.vue";
 import LoadingIndicator from "./LoadingIndicator.vue";
 
 // register lib components
 Vue.component("loading-indicator", LoadingIndicator);
 Vue.component("generic-activated-dialog-card", GenericActivatedDialogCard);
+Vue.component("fab-activated-dialog-card", FabActivatedDialogCard);
