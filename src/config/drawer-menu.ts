ae859bde2eea0706d80b41b66f9c93304dec52d5
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2019-08-31 19:37:11 +0200

    fix: rename MDs label to professionals

--- src/config/drawer-menu.ts
+++ src/config/drawer-menu.ts
@@ -4,5 +4,5 @@ export const drawerMenu = [
     { link: routes.list, text: "Lista porad", icon: "fa-list" },
     { link: routes.sendAdvice, text: "Importuj poradę", icon: "fa-tachometer-alt" },
     { link: routes.discussion, text: "Dyskusja & pytania", icon: "fa-comments" },
-    { link: routes.medicalprofessional, text: "Lista lekarzy", icon: "fa-user-nurse" },
+    { link: routes.medicalprofessional, text: "Lista specjalistów", icon: "fa-user-nurse" },
 ];
