6ae1b1916dfa10deb981a810321050f176a61695
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2019-09-01 20:54:17 +0200

    feat: use ResourceStatus in AugmentedStatusComponent

--- src/components/common/AugmentedStatusComponent.vue
+++ src/components/common/AugmentedStatusComponent.vue
@@ -1,21 +1,21 @@
-<template v-if="loading || success || error">
+<template v-if="show">
     <v-layout wrap>
-        <v-flex xs12 text-center class="my-2">LOADING LIST</v-flex>
-        <v-flex v-if="loading" xs12 text-center class="my-2">
+        <v-flex v-if="this.resource.loading" xs12 text-center class="my-2">
             <span>Loading</span>
             <loading-indicator />
         </v-flex>
-        <v-flex v-if="error" xs12 text-center class="my-2">
-            <AugmentedErrorComponent :locale="locale" :error="error" />
+        <v-flex v-if="hasError" xs12 text-center class="my-2">
+            <AugmentedErrorComponent :locale="locale" :error="this.resource.error" />
         </v-flex>
         <v-flex v-if="success" xs12 text-center class="my-2">
-            <v-alert type="success">{{ success }}</v-alert>
+            <v-alert type="success">{{ successText }}</v-alert>
         </v-flex>
     </v-layout>
 </template>
 
 <script lang="ts">
     import { visualConfig } from "@/global";
+    import { ResourceStatus } from "@/util/ResourceStatus";
     import { AugmentedLocalizedError } from "localized-error";
     import Vue from "vue";
 
@@ -23,14 +23,25 @@
 
     export default Vue.extend({
         props: {
-            loading: Boolean,
-            success: String,
-            error: [String, Object as () => AugmentedLocalizedError<string> | Error],
+            resource: () => ResourceStatus,
+            successText: String,
         },
         data() {
             return {
                 locale: visualConfig.locale,
             };
+        },
+        computed: {
+            success(): boolean {
+                  return !!this.successText && ResourceStatus.isSuccess(this.resource);
+            },
+            hasError(): boolean {
+                return  ResourceStatus.hasError(this.resource);
+            },
+            show(): boolean {
+                return this.resource.loading || this.hasError || this.success;
+            },
+
         },
         components: {
             AugmentedErrorComponent,
