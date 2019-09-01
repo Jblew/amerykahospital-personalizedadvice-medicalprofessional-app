e1fc19cc96ae995c7addda18d7bec8ed09f4c9ec
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2019-09-01 20:55:04 +0200

    feat: create action builders

---
+++ src/util/action_utils.ts
@@ -0,0 +1,26 @@
+import { Action as VuexAction, ActionContext as VuexActionContext, ActionTree, Dispatch } from "vuex";
+
+type ActionFn<STATE_TYPE> = VuexAction<STATE_TYPE, STATE_TYPE>;
+type ActionContext<STATE_TYPE> = VuexActionContext<STATE_TYPE, STATE_TYPE>;
+
+export type DispatcherFn<PAYLOAD_TYPE> = (dispatchFn: Dispatch, payload: PAYLOAD_TYPE) => ReturnType<Dispatch>;
+
+export function dispatcher<PAYLOAD_TYPE>(actionName: string): DispatcherFn<PAYLOAD_TYPE> {
+    return (dispatchFn: Dispatch, payload: PAYLOAD_TYPE) => dispatchFn(actionName, payload);
+}
+
+export type GenericDeclaration<PAYLOAD_TYPE, STATE_TYPE>
+    = ActionFn<STATE_TYPE> & ((c: ActionContext<STATE_TYPE>, medicalProfessional: PAYLOAD_TYPE) => void);
+
+export function moduleActionRegistratorFactory<STATE_TYPE>(moduleName: string) {
+    return <PAYLOAD_TYPE>(actionName: string) => {
+        const dispatch = dispatcher<PAYLOAD_TYPE>(actionName);
+        const register = (
+            actionTree: ActionTree<STATE_TYPE, STATE_TYPE>, fn: GenericDeclaration<PAYLOAD_TYPE, STATE_TYPE>,
+        ) => {
+            const actionPath = `${moduleName}_${actionName}`;
+            actionTree[actionPath] = fn;
+        };
+        return Object.freeze({ dispatch, register });
+    };
+}
